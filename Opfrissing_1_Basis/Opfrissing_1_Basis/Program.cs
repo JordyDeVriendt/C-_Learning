8af9828c9f3763e518c7cf4012ea7f1001188f96
Jordy <jordydevriendt@outlook.com>
2026-02-04 16:52:10 +0100

    Basis classes, for loops etc

---
+++ Opfrissing_1_Basis/Opfrissing_1_Basis/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
+List<string> friends = new List<string>();
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Enter friend {i + 1} name: ");
+    friends.Add(Console.ReadLine());
+}
+
+Console.WriteLine("\nYour friends are:");
+for (int i = 0;i < 10; i++)
+{
+    Console.WriteLine($"{i + 1}. {friends[i]}");
+}
